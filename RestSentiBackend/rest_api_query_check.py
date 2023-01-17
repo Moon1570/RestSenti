8ae600955ebe06919e4caab44f6979c84583ee0f
aburifat <rifat.cse4.bu@gmail.com>
2023-01-17 15:52:01 +0600

    POST API REQUEST EXAMPLE

--- RestSentiBackend/rest_api_query_check.py
+++ RestSentiBackend/rest_api_query_check.py
@@ -1,22 +1,13 @@
 import requests
 
-url = 'http://127.0.0.1:8000/sa/new/'
-url2 = 'http://127.0.0.1:8000/sa/records/105'
+url = 'https://todo.aburifat.com/sa/new/'
+url2 = 'http://127.0.0.1:8000/sa/new/'
 myobj = {
     'uid': '105',
-    'sentance': 'Why you are?'
+    'sentance': 'Because I\'m Happy!'
 }
 
-myobj2 = {
-    'uid': [
-        '101'
-    ],
-    'sentance': [
-        'I\'m happy.'
-    ]
-}
 
 x = requests.post(url, json = myobj)
-#y = requests.get(url2)
 
 print(x.text)
